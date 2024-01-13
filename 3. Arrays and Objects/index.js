a863cd701e8a3950066804c5c17b48792357d202
John Wilbert Laiño <154640046+john-wilbert@users.noreply.github.com>
2024-01-13 02:41:21 +0000

    done 3.1

--- 3. Arrays and Objects/index.js
+++ 3. Arrays and Objects/index.js
@@ -14,7 +14,7 @@ let numbers = [
 
 // Todo 3.1 Get the first, 5th, and last items in the numbers array.
 // Your code here
-
+console.log(numbers[0], numbers[4], numbers[numbers.length-1]);
 // Todo 3.2 calculate the min, max, and the average of the numbers array
 // Your code here
 
