3a1c142dd0e1a0527e6eaeccc5202428c370a94c
John Wilbert Laiño <154640046+john-wilbert@users.noreply.github.com>
2024-01-13 02:36:05 +0000

    done m2

--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -7,18 +7,39 @@ const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock
 
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 // Your code here
-
+if (randomNumber > 50) {
+    console.log("more than fifty")
+}
+else {
+    console.log("less than fifty")
+}
 // TODO 2.2 Display whether the random number is odd or even
 // Your code here
-
+if (randomNumber % 2 == 0) {
+    console.log(randomNumber + " is even")
+}
+else {
+    console.log(randomNumber + " is odd")
+}
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
+if (randomNumber % 3 == 0) {
+    console.log("fizz")
+} else if (randomNumber % 5 == 0) {
+    console.log("buzz")
+} else if (randomNumber % 3 == 0 && randomNumber % 5 == 0) {
+    console.log("fizzbuzz")
+}
+else {
+    console.log(randomNumber)
+}
 
-let toDisplay = ""
+
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
+let toDisplay = randomNumber % 2 == 0 ? "Even" : "Odd"
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
@@ -27,30 +48,58 @@ console.log("toDisplay", toDisplay);
 // TODO 2.5 Use a for loop to print the numbers 1 to N
 const n = 10
 // Your code here
+for(let i = 1; i <= n; i++){
+    console.log(i)
+}
+
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
 // Your code here
+console.log("While Loop: ");
+let index = 0
+while(index < list.length){
+    console.log(list[index]);
+    index++
+}
 
 // Checkpoint 2.3 How would you simulate a do-while loop in JavaScript
 // Answer: 
 
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
-
+console.log("For Of:");
+for(const i of list){
+    console.log(i)
+}
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
+console.log("For In:");
+for(const i in list){
+    console.log(list[i])
+}
 
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
-
+console.log("For Each:")
+list.forEach((element) => console.log(element))
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
 // Answer: 
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
+console.log("Try Catch");
 const numerator = Math.floor((Math.random() * 100) + 1);
 const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
+try{
+    const quotient = numerator / denominator;
+    if(denominator == 0){
+        throw Error("Division by zero error")
+    }
+}catch(error){
+    console.log("ERROR");
+}finally{
+    console.log("cleaning up resources");
+}
 
-const quotient = numerator / denominator;
 
